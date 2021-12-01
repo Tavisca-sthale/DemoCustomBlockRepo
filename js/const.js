dceb045451329a54339d94d3dd2322505d502ef3
Tavisca-sthale <91875953+Tavisca-sthale@users.noreply.github.com>
2021-12-01 16:33:42 +0530

    Replaced dist folder

--- js/const.js
+++
@@ -1,5 +0,0 @@
-export const BLOCK_UNIQUE_CLASS_NAME = `esd-alert-block`;
-
-export const CONTROL_NAME_PREFIX = `demoAlertBlock`;
-export const CONTROL_NAME_BLOCK_BACKGROUND_COLOR = `${CONTROL_NAME_PREFIX}_backgroundColor_control`;
-export const CONTROL_NAME_TITLE_TEXT_COLOR = `${CONTROL_NAME_PREFIX}_titleTextColor_control`;
